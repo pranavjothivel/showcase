47ef6a380e067be344ad25604b8230a1b0b46603
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2026-03-29 00:26:49 -0400

    rename: file extensions to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['40ed-198-44-159-86.ngrok-free.app'],
+  allowedDevOrigins: ['*.ngrok-free.app'],
   experimental: {
     optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react']
   },
